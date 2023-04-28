1e10b432680dc9b2e40a7e3beb0c85c8972b747d
Ckster <erve3705@colorado.edu>
2023-04-28 15:07:21 -0600

    No more rasterio

--- fdeo/api.py
+++ fdeo/api.py
@@ -16,6 +16,7 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 from osgeo import osr
+import ogr
 import gdal
 import numpy as np
 from pyhdf.SD import SD, SDC
@@ -444,22 +445,30 @@ class EVI(BaseAPI):
 
         tiff_file = self._numpy_array_to_raster(output_tif_file, array, geotransform, 'wgs84')
 
-        # Open the raster file
-        with rasterio.open(tiff_file, encoding='latin-1') as src:
-            with open(os.path.join(self.PROJ_DIR, 'data', 'CONUS_WGS84.geojson')) as f:
-                geojson = json.load(f)
-            polygon = gpd.GeoDataFrame.from_features(geojson['features'])
-
-            # Extract the data using the polygon to create a mask
-            out_image, out_transform = mask(src, polygon.geometry, nodata=src.nodata, crop=True)
-            print(out_image.astype('uint32'))
-            # Update the metadata of the output tif file
-            out_meta = src.meta.copy()
-
-            # Open the GeoJSON file containing the polygon
-
-            out_meta.update({"driver": "GTiff", "height": out_image.shape[1], "width": out_image.shape[2],
-                             "transform": out_transform, "dtype": 'uint32'})
-            # Write the clipped tif file to disk
-            with rasterio.open(tiff_file.replace('.tif', '_conus.tif'), "w", **out_meta) as dest:
-                dest.write(out_image.astype('uint32'))
+        # Open the input tif file
+        input_ds = gdal.Open(tiff_file)
+        # Load the polygon from the geojson file
+        with open(os.path.join(self.PROJ_DIR, 'data', 'CONUS_WGS84.geojson')) as f:
+            geojson = f.read()
+        polygon = ogr.CreateGeometryFromJson(geojson)
+        # Get the extent of the polygon
+        minX, maxX, minY, maxY = polygon.GetEnvelope()
+        # Get the GeoTransform of the input tif file
+        input_gt = input_ds.GetGeoTransform()
+        # Calculate the offset and size of the output tif file
+        x_offset = int((minX - input_gt[0]) / input_gt[1])
+        y_offset = int((maxY - input_gt[3]) / input_gt[5])
+        x_size = int((maxX - minX) / input_gt[1])
+        y_size = int((maxY - minY) / input_gt[5])
+        # Read the data from the input tif file
+        band = input_ds.GetRasterBand(1)
+        data = band.ReadAsArray(x_offset, y_offset, x_size, y_size)
+        # Create the output tif file
+        driver = gdal.GetDriverByName('GTiff')
+        output_ds = driver.Create(tiff_file.replace('.tif', '_conus.tif'), x_size, y_size, 1, gdal.GDT_UInt32)
+        output_ds.SetGeoTransform((minX, input_gt[1], 0, maxY, 0, input_gt[5]))
+        output_ds.SetProjection(input_ds.GetProjection())
+        # Write the data to the output tif file
+        band_out = output_ds.GetRasterBand(1)
+        band_out.WriteArray(data.astype('uint32'))
+        band_out.FlushCache()
