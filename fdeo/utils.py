c933a951f40e05fd7a0940ecc06def662a985f0a
Ckster <erve3705@colorado.edu>
2023-05-01 23:01:02 -0600

    Switching lats and lons

--- fdeo/utils.py
+++ fdeo/utils.py
@@ -29,14 +29,12 @@ def set_tiff_resolution(input_resolution_path: str, target_resolution_path: str)
 
     input_res_lons, input_res_lats = get_geo_locations_from_tif(input_res)
 
-    print(input_res_data.shape)
-
-    input_res_interp = RegularGridInterpolator((input_res_lons, input_res_lats), input_res_data, method='linear')
+    input_res_interp = RegularGridInterpolator((input_res_lats, input_res_lons), input_res_data, method='linear')
 
     target_res = gdal.Open(target_resolution_path)
 
     target_res_lons, target_res_lats = get_geo_locations_from_tif(target_res)
-    target_res_data = input_res_interp(target_res_lons, target_res_lats)
+    target_res_data = input_res_interp(target_res_lats, target_res_lons)
 
     return target_res_data
 
